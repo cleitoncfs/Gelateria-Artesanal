56f85557a0c38d7d7e9af667d51114aa72a8b728
cleitoncfs <cleitoncfs@gmail.com>
2025-05-15 17:17:52 +0100

    Update Header

--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -14,43 +14,47 @@ export default function Topo() {
 
     return (
         <header className={estilos.topo_container}>
-            <div>
+            <div className={estilos.wrapper}>
+                {/* Esquerda: Logo */}
                 <img
                     className={estilos.img}
                     src={Logo}
                     alt="Logomarca da Gelateria Artesanal"
                 />
 
-                {/* Botão hambúrguer visível no mobile */}
-                <button
-                    className={`${estilos.menu_botao} ${
-                        menuAberto ? estilos.aberto : ""
-                    }`}
-                    onClick={() => setMenuAberto(!menuAberto)}
-                    aria-label="Abrir ou fechar menu"
-                >
-                    <span className={estilos.hamburguer}></span>
-                    <span className={estilos.hamburguer}></span>
-                    <span className={estilos.hamburguer}></span>
-                </button>
+                {/* Direita: Botão e/ou navegação */}
+                <div className={estilos.menu_area}>
+                    {/* Navegação */}
+                    <nav
+                        className={`${estilos.navegacao} ${
+                            menuAberto ? estilos.menu_ativo : ""
+                        }`}
+                        aria-label="Navegação principal"
+                    >
+                        <Link className={estilos.link_topo} to="/">
+                            Home
+                        </Link>
+                        <Link className={estilos.link_topo} to="/sabores">
+                            Sabores
+                        </Link>
+                        <Link className={estilos.link_topo} to="/sobre">
+                            Sobre
+                        </Link>
+                    </nav>
 
-                {/* Navegação com menu ativo controlado por classe */}
-                <nav
-                    className={`${estilos.navegacao} ${
-                        menuAberto ? estilos.menu_ativo : ""
-                    }`}
-                    aria-label="Navegação principal"
-                >
-                    <Link className={estilos.link_topo} to="/">
-                        Home
-                    </Link>
-                    <Link className={estilos.link_topo} to="/sabores">
-                        Sabores
-                    </Link>
-                    <Link className={estilos.link_topo} to="/sobre">
-                        Sobre
-                    </Link>
-                </nav>
+                    {/* Botão hambúrguer */}
+                    <button
+                        className={`${estilos.menu_botao} ${
+                            menuAberto ? estilos.aberto : ""
+                        }`}
+                        onClick={() => setMenuAberto(!menuAberto)}
+                        aria-label="Abrir ou fechar menu"
+                    >
+                        <span className={estilos.hamburguer}></span>
+                        <span className={estilos.hamburguer}></span>
+                        <span className={estilos.hamburguer}></span>
+                    </button>
+                </div>
             </div>
         </header>
     );
